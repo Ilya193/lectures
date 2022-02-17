59e52bf68d180986652053bd98d537f42ab9beab
ilya <ilya.komendantov@list.ru>
2022-02-17 11:08:05 +0300

    lecture3

--- app/src/main/java/com/xlwe/lectures/MainActivity.kt
+++ app/src/main/java/com/xlwe/lectures/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.xlwe.lectures
 
-import android.content.Context
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.xlwe.lectures.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +17,83 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val textWatcher = object : SimpleTextWatcher() {
+        override fun afterTextChanged(s: Editable?) {
+            val input = s.toString()
+            if (input.endsWith("@g")) {
+                val full = "${input}mail.com"
+                setText(full)
+            }
+        }
+    }
+
+    private fun setText(full: String) {
+        binding.textInputEditTextLogin.removeTextChangedListener(textWatcher)
+        binding.textInputEditTextLogin.setTextCorrectly(full)
+        binding.textInputEditTextLogin.addTextChangedListener(textWatcher)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //binding.textInputEditText.addTextChangedListener(textWatcher)
+        binding.textInputEditTextLogin.listenChanges {
+            binding.textInputLayoutLogin.isErrorEnabled = false
+        }
 
+        binding.textInputEditTextPassword.filters = arrayOf(InputFilter.LengthFilter(MAX_LENGTH))
+
+        binding.loginButton.setOnClickListener {
+            val valid =
+                android.util.Patterns.EMAIL_ADDRESS.matcher(binding.textInputEditTextLogin.text.toString()).matches()
+
+            if (valid) {
+                hideKeyboard(binding.textInputEditTextLogin)
+                binding.loginButton.isEnabled = false
+                Snackbar.make(
+                    binding.loginButton, getString(R.string.valid_email_message),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+            else {
+                binding.textInputLayoutLogin.isErrorEnabled = true
+                binding.textInputLayoutLogin.error = getString(R.string.invalid_email_message)
+            }
+        }
     }
+
+    companion object {
+        const val MAX_LENGTH = 10
+    }
+
+    private fun log(message: String) {
+        Log.d("MainActivity", message)
+    }
+}
+
+fun AppCompatActivity.hideKeyboard(view: View) {
+    val imm = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun TextInputEditText.listenChanges(block: (text: String) -> Unit) {
+    addTextChangedListener(object : SimpleTextWatcher() {
+        override fun afterTextChanged(s: Editable?) {
+            block.invoke(s.toString())
+        }
+    })
+}
+
+fun TextInputEditText.setTextCorrectly(text: String) {
+    setText(text)
+    setSelection(text.length)
+}
+
+abstract class SimpleTextWatcher : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+    override fun afterTextChanged(s: Editable?) {}
 }
